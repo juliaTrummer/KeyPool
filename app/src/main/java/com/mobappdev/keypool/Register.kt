cc3bc29e18e72c080fc9369d5e7a5ce91d107d40
truju <julia.trummer@edu.fh-joanneum.at>
2021-09-27 23:23:54 +0200

    added icon, validation

--- app/src/main/java/com/mobappdev/keypool/Register.kt
+++ app/src/main/java/com/mobappdev/keypool/Register.kt
@@ -1,21 +1,34 @@
 package com.mobappdev.keypool
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.animation.Animation
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.mobappdev.keypool.RegisterLogin.EmailPasswordActivity
 import com.mobappdev.keypool.RegisterLogin.Login
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 
 class Register : AppCompatActivity() {
 
+    val REGEX : Regex = Regex("(^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%!\\-_?&])(?=\\S+\$).{8,})")
+
     private lateinit var email : TextInputEditText
+    private lateinit var emailField : TextInputLayout
     private lateinit var username : TextInputEditText
     private lateinit var password : TextInputEditText
+    private lateinit var passwordField : TextInputLayout
     private lateinit var button : Button
     private lateinit var infoText : TextView
+    private lateinit var emailPasswordActivity: EmailPasswordActivity
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +36,17 @@ class Register : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         email = findViewById(R.id.email)
+        emailField = findViewById(R.id.emailField)
         username = findViewById(R.id.username)
         password = findViewById(R.id.password)
+        passwordField = findViewById(R.id.passwordField)
         button = findViewById(R.id.registerButton)
         infoText = findViewById(R.id.loginInfoText)
 
-        button.setOnClickListener(){
+        emailPasswordActivity = EmailPasswordActivity()
 
+        button.setOnClickListener(){
+            register()
         }
 
         infoText.setOnClickListener(){
@@ -39,9 +56,46 @@ class Register : AppCompatActivity() {
     }
 
     private fun register(){
-        //validate
+        var validEmail : Boolean = validateEmail(email.text.toString())
+        var strongPW : Boolean = validatePassword(password.text.toString())
+        if(strongPW && validEmail){
+            passwordField.error = null
+            emailField.error = null
+            emailPasswordActivity.createAccount(password.text.toString(), email.text.toString())
+        } else if(strongPW){
+            passwordField.error = null
+        } else if(validEmail){
+            emailField.error = null
+        }
         //register
         //toast
+        Toast.makeText(this, "Register is a success.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun validateUserName(){
+
+
+    }
+
+    //create better Messages
+    fun validatePassword(input: CharSequence): Boolean {
+        if(password.text!=null){
+            if(!REGEX.containsMatchIn(input)){
+                passwordField.error = "Password must contain numbers, symbols and letters!"
+                return false
+            }
+        }
+        return true
+    }
+
+    fun validateEmail(input: CharSequence): Boolean {
+        if(email.text!=null){
+            if(!(input.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(input).matches())){
+                emailField.error = "Please enter a valid Email!"
+                return false
+            }
+        }
+        return true
     }
 
 }
--- app/src/main/java/com/mobappdev/keypool/RegisterLogin/EmailPasswordActivity.kt
+++ app/src/main/java/com/mobappdev/keypool/RegisterLogin/EmailPasswordActivity.kt
@@ -1,41 +1,39 @@
 package com.mobappdev.keypool.RegisterLogin
 
-import com.mobappdev.keypool.R
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
-class EmailPasswordActivity : AppCompatActivity() {
+class EmailPasswordActivity : Activity() {
 
     private lateinit var auth: FirebaseAuth
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-
-        auth = FirebaseAuth.getInstance()
     }
 
     public override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         if(currentUser != null){
             reload();
         }
     }
 
-    private fun createAccount(email: String, password: String) {
+    fun createAccount(email: String, password: String) {
+        auth = FirebaseAuth.getInstance()
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
+                    // If sign in fails, display a message to the user.
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
@@ -45,7 +43,6 @@ class EmailPasswordActivity : AppCompatActivity() {
     }
 
     private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -61,7 +58,6 @@ class EmailPasswordActivity : AppCompatActivity() {
                     updateUI(null)
                 }
             }
-        // [END sign_in_with_email]
     }
 
     private fun sendEmailVerification() {
